94d7bc276588e7c3120de875222c69809160f88a
Junhao Li <streaver91@gmail.com>
2017-08-15 14:54:00 -0400

    performance tuned

--- pi.cc
+++ pi.cc
@@ -19,28 +19,24 @@ int main(int argc, char** argv) {
 
 #ifdef NUM_THREADS
   omp_set_num_threads(NUM_THREADS);
-#else
-  omp_set_num_threads(omp_get_num_procs());
 #endif
 
 #pragma omp parallel
   {
-    double thread_sum = 0.0;
     if (id == 0 && omp_get_thread_num() == 0) {
       printf("Number of procs: %d\n", n);
       printf("Number of threads: %d\n", omp_get_num_threads());
     }
-#pragma omp for
+#pragma omp for reduction (+:local_sum) schedule(static, 1)
     for (int i = id; i < N; i += n) {
       if (i == 0) continue;
       double term = 4.0 / (2 * i - 1);
+      for (int j = 0; j < 20; j++) term = sin(term);
       if ((i & 1) == 0) {
         term = -term;
       }
-      thread_sum += sin(term);
+      local_sum += sin(term);
     }
-#pragma omp atomic
-    local_sum += thread_sum;
   }
 
   boost::mpi::reduce(world, local_sum, global_sum, std::plus<double>(), 0);
